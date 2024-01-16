dae3a02ad57c0cbf5fd8a7263ca1db42f3d1e631
jononaev <jononaevanush@gmail.com>
2024-01-16 21:25:18 +0500

    added npm publish

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
+/*console.log("Welcome to the Brain Games!");*/
+/*console.log("May I have your name? John");*/
+/*console.log("Hello, John!");*/
 
+import getBirthYear from "./src/cli.js";
+
+const user = "";
 console.log("Welcome to the Brain Games!");
+const name = getBirthYear(user);
+console.log("Hello, " + name + "!");
